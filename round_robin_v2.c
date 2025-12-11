4c89dd72a93fae68169568e55b41dd842f6d8d32
yoru <jordanchandra1@gmail.com>
2025-12-11 16:44:04 +0700

    feat: create process

--- round_robin_v2.c
+++ round_robin_v2.c
@@ -8,11 +8,29 @@
 #define NUM_TASK 3
 #define TIME_SLICE 2  // seconds
 
+int current_task = 0;
+pid_t tasks[NUM_TASK];
+
+void timer_handler();
+void run_task(int task_id);
+
 int main() {
     printf("Time-Sharing System Simulation\n\n");
 
     // create child processes (tasks)
-    
+    for (int i = 0; i < NUM_TASK; i++) {
+        pid_t pid = fork();
+        if (pid == 0) {
+            // child process
+            run_task(i);
+            exit(0);
+        } else {
+            tasks[i] = pid;
+            // initially stop all tasks except the first one
+            if (i != 0)
+                kill(tasks[i], SIGSTOP);
+        }
+    }
 
     return 0;
 }
