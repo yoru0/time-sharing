137d8634446499df5bc36f7436f9c4833107d9ba
yoru <jordanchandra1@gmail.com>
2025-12-11 16:39:32 +0700

    feat: init

---
+++ round_robin_v2.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <signal.h>
+#include <unistd.h>
+#include <sys/time.h>
+#include <sys/wait.h>
+
+#define NUM_TASK 3
+#define TIME_SLICE 2  // seconds
+
+int main() {
+    printf("Time-Sharing System Simulation\n\n");
+
+    // create child processes (tasks)
+    
+
+    return 0;
+}
